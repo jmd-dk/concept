2c96f9ae0b3f048412cbf07920bdf17c37d5add1
jmd-dk <jeppe_dakin@hotmail.com>
2015-01-12 00:05:16 +0100

    Comitted from /home/jeppe/Documents/Uni/Speciale/Nbody0.1

--- source/timeloop.py
+++ source/timeloop.py
@@ -36,18 +36,18 @@ cython.declare(particles='Particles')
 particles = Particles(pos=random((3, N))*boxsize, vel=0.7*(2*random((3, N))-1), mass=1)
 
 # Setting up figure and plot the particles
-if master:
-    fig = figure()
-    ax = fig.add_subplot(111, projection='3d')
-    h = ax.scatter(*particles.pos, color='purple', alpha=0.25)
-    gca().set_xlim3d(0, boxsize)
-    gca().set_ylim3d(0, boxsize)
-    gca().set_zlim3d(0, boxsize)
-    gca().set_xlabel(r'$x$')
-    gca().set_ylabel(r'$y$')
-    gca().set_zlabel(r'$z$')
+#if master:
+#    fig = figure()
+#    ax = fig.add_subplot(111, projection='3d')
+#    h = ax.scatter(*particles.pos, color='purple', alpha=0.25)
+#    gca().set_xlim3d(0, boxsize)
+#    gca().set_ylim3d(0, boxsize)
+#    gca().set_zlim3d(0, boxsize)
+#    gca().set_xlabel(r'$x$')
+#    gca().set_ylabel(r'$y$')
+#    gca().set_zlabel(r'$z$')
     #ax.view_init(90, 0)
-    show(block=False)
+#    show(block=False)
 
 # Run main loop
 cython.declare(i='size_t')
@@ -56,6 +56,6 @@ for i in range(10):
     PP(particles)  # This should be a method on particles! It should be able to choose between PP and PM when it is instantiated
     print(time() - t0)
     particles.drift()
-    if master:
-        h._offsets3d = juggle_axes(*particles.pos, zdir='z')
-        draw()
+    #if master:
+    #    h._offsets3d = juggle_axes(*particles.pos, zdir='z')
+    #    draw()
