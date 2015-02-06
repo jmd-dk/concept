a06a34ca508da68f5334917a2409dd8c21f74309
jmd-dk <jeppe_dakin@hotmail.com>
2015-02-06 11:34:50 +0100

    proper test conditions

--- source/timeloop.py
+++ source/timeloop.py
@@ -19,7 +19,7 @@ else:
 
 # Construct
 cython.declare(particles='Particles')
-particles = construct_random('some typename', 'dark matter', N=20000)
+particles = construct_random('some typename', 'dark matter', N=500)
 particles.mass = 3*H0**2/(8*pi*G_Newton)*boxsize**3/particles.N
 # Save
 save(particles, 'ICs/test')
