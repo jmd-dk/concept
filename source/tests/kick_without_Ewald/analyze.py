92e3d68febcd7f02defc51560ebc933f71940c5d
jmd-dk <jeppe_dakin@hotmail.com>
2015-03-03 01:27:49 +0100

    added kick_without_Ewald test

---
+++ source/tests/kick_without_Ewald/analyze.py
@@ -0,0 +1,85 @@
+# This file has to be run in pure Python mode!
+
+# Include the code directory in the searched paths
+import sys, os
+Nbody_dir = os.path.realpath(__file__)
+this_dir = os.path.dirname(Nbody_dir)
+while True:
+    if Nbody_dir == '/':
+        raise Exception('Cannot find the .paths file!')
+    if '.paths' in os.listdir(os.path.dirname(Nbody_dir)):
+        break
+    Nbody_dir = os.path.dirname(Nbody_dir)
+sys.path.append(Nbody_dir)
+
+# Imports from the N-body code
+from commons import *
+from IO import Gadget_snapshot
+
+# Use a matplotlib backend that does not require a running X-server
+import matplotlib
+matplotlib.use('TkAgg')
+import matplotlib.pyplot as plt
+
+N_snapshots = 5
+snapshot = Gadget_snapshot()
+
+# Read in data from the N-body snapshots
+a = zeros(N_snapshots)
+x0 = zeros(N_snapshots)
+x0_std = zeros(N_snapshots)
+x1 = zeros(N_snapshots)
+x1_std = zeros(N_snapshots)
+for i in range(N_snapshots):
+    snapshot.load(this_dir + '/output/snapshot_' + str(i))
+    a[i] = snapshot.header['Time']
+    x0[i] = np.mean(snapshot.particles.posx[:4])
+    x0_std[i] = np.std(snapshot.particles.posx[:4])
+    x1[i] = np.mean(snapshot.particles.posx[4:])
+    x1_std[i] = np.std(snapshot.particles.posx[4:])
+
+# Read in data from the Gadget snapshots
+a_gadget = zeros(N_snapshots)
+x0_gadget = zeros(N_snapshots)
+x0_std_gadget = zeros(N_snapshots)
+x1_gadget = zeros(N_snapshots)
+x1_std_gadget = zeros(N_snapshots)
+for i in range(N_snapshots):
+    snapshot.load(this_dir + '/output/snapshot_gadget_' + '0'*(3-len(str(i))) + str(i))
+    a_gadget[i] = snapshot.header['Time']
+    x_gadget = snapshot.particles.posx[np.argsort(snapshot.ID)]
+    x0_gadget[i] = np.mean(x_gadget[:4])
+    x0_std_gadget[i] = np.std(x_gadget[:4])
+    x1_gadget[i] = np.mean(x_gadget[4:])
+    x1_std_gadget[i] = np.std(x_gadget[4:])
+
+# Plot
+fig_file = this_dir + '/result.pdf'
+plt.errorbar(a, x0/units.kpc, yerr=x0_std/units.kpc, fmt='-or', label='$N$-body (left)')
+plt.errorbar(a, x1/units.kpc, yerr=x1_std/units.kpc, fmt='-dr', label='$N$-body (right)')
+plt.errorbar(a_gadget, x0_gadget/units.kpc, yerr=x0_std_gadget/units.kpc, fmt='--3b', label='Gadget (left)')
+plt.errorbar(a_gadget, x1_gadget/units.kpc, yerr=x1_std_gadget/units.kpc, fmt='--4b', label='Gadget (right)')
+plt.legend(loc='best')
+plt.xlabel('$a$')
+plt.ylabel(r'$x\,\mathrm{[kpc]}$')
+plt.ylim(0, boxsize)
+plt.savefig(fig_file)
+
+# Analyze
+# There should be no variance on the x positions
+tol = N_snapshots*100*np.finfo('float32').eps
+if np.sum(x0_std_gadget) > tol or np.sum(x1_std_gadget) > tol:
+    print('\033[1m\033[91m' + 'Unequal x-positions for the 2*4 particles in the Gadget snapshots.\n'
+          + 'It is no good to compare the N-body results to these.' + '\033[0m')
+    sys.exit(1)
+if np.sum(x0_std) > tol or np.sum(x1_std) > tol:
+    print('\033[1m\033[91m' + 'Unequal x-positions for the 2*4 particles in the snapshots.\n'
+          + 'The symmetric initial conditions has produced nonsymmetric results!' + '\033[0m')
+    sys.exit(1)
+# Compare N-body to Gadget
+tol = 1e-3
+if max(np.abs(x0/x0_gadget - 1)) > tol or max(np.abs(x1/x1_gadget - 1)) > tol:
+    print('\033[1m\033[91m' + 'The results from the N-body code disagree with those from Gadget.\n'
+          + 'See ' + fig_file + ' for a visualization.' + '\033[0m')
+    sys.exit(1)
+
---
+++ source/tests/kick_without_Ewald/generate_IC.py
@@ -0,0 +1,32 @@
+# This file has to be run in pure Python mode!
+
+# Include the code directory in the searched paths
+import sys, os
+Nbody_dir = os.path.realpath(__file__)
+this_dir = os.path.dirname(Nbody_dir)
+while True:
+    if Nbody_dir == '/':
+        raise Exception('Cannot find the .paths file!')
+    if '.paths' in os.listdir(os.path.dirname(Nbody_dir)):
+        break
+    Nbody_dir = os.path.dirname(Nbody_dir)
+sys.path.append(Nbody_dir)
+
+# Imports from the N-body code
+from commons import *
+from species import construct
+from IO import save
+
+# Create the particles
+N = 8
+mass = Ωm*ϱ*boxsize**3/N
+particles = construct('kick_without_Ewald test', 'dark matter', mass, N)
+particles.populate(array([0.25 + 0.01]*4 + [0.75 - 0.01]*4)*boxsize, 'posx')
+particles.populate(array([0.25, 0.25, 0.75, 0.75]*2)*boxsize, 'posy')
+particles.populate(array([0.25, 0.75, 0.75, 0.25]*2)*boxsize, 'posz')
+particles.populate(zeros(N), 'momx')
+particles.populate(zeros(N), 'momy')
+particles.populate(zeros(N), 'momz')
+
+# Save snapshot
+save(particles, a_begin, IC_file)
