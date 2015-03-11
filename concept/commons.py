767a8ff1d025a25da94ce2ab4b24f813cd4e9e17
jmd-dk <jeppe_dakin@hotmail.com>
2015-03-12 00:40:00 +0100

    removed attempt on individual time steps

--- concept/commons.py
+++ concept/commons.py
@@ -131,8 +131,6 @@ cython.declare(G_Newton='double',
                half_boxsize='double',
                machine_ϵ='double',
                minus_half_boxsize='double',
-               softening='double',
-               softening2='double',
                two_ewald_gridsize='int',
                two_machine_ϵ='double',
                two_recp_boxsize='double',
@@ -142,7 +140,7 @@ cython.declare(G_Newton='double',
                )
 G_Newton = 6.6738e-11*units.m**3/units.kg/units.s**2  # Newtons constant
 ϱ = 3*H0**2/(8*pi*G_Newton) # The average, comoing density (the critical comoving density since we only study flat universes)
-softening = 30/0.7*units.kpc  # A few percent0.02*boxsize/(N**one_third). Source: http://popia.ft.uam.es/aknebe/page3/files/ComputationalAstrophysics/PhysicalProcesses.pdf page 85 and http://www.ast.cam.ac.uk/~puchwein/NumericalCosmology02.pdf page 13.
+#softening = 30/0.7*units.kpc  # A few percent0.02*boxsize/(N**one_third). Source: http://popia.ft.uam.es/aknebe/page3/files/ComputationalAstrophysics/PhysicalProcesses.pdf page 85 and http://www.ast.cam.ac.uk/~puchwein/NumericalCosmology02.pdf page 13.
 PM_gridsize3 = PM_gridsize**3
 boxsize2 = boxsize**2
 half_boxsize = 0.5*boxsize
@@ -153,6 +151,7 @@ machine_ϵ = np.finfo('float64').eps  # Machine epsilon
 two_ewald_gridsize = 2*ewald_gridsize
 two_machine_ϵ = 2*machine_ϵ
 use_PM = True  # Flag specifying whether the PM method is used or not. THIS SHOULD BE COMPUTED BASED ON PARTICLES CHOSEN IN THE PARAMETER FILE!!!!!!!!!!!
+
 
 
 #############
