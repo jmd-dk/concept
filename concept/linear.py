7c45c806bbf9519e4d082f75508548c3fb0241db
Jeppe Dakin <jeppe_dakin@hotmail.com>
2019-03-04 22:32:41 +0100

    reduced maxfev to 1_000 (default is 200)

--- concept/linear.py
+++ concept/linear.py
@@ -1858,7 +1858,7 @@ class TransferFunction:
                                 ftol=1e-12,
                                 xtol=1e-12,
                                 gtol=1e-12,
-                                maxfev=10_000,
+                                maxfev=1_000,
                             )
                         )
                     except:
