8b1967777ec285d754b3d3452ca9b2b024f391cd
Jeppe Dakin <jeppe_dakin@hotmail.com>
2020-01-30 12:46:09 +0100

    added sphinx_copybutton to documentation and installation

--- concept/docs/conf.py
+++ concept/docs/conf.py
@@ -1,38 +1,45 @@
 # Project
-project = 'CONCEPT'
+project   = 'CONCEPT'
+author    = 'Jeppe Dakin'
 copyright = '2019, Jeppe Dakin'
-author = 'Jeppe Dakin'
 
 # Paths
 html_static_path = ['_static']
 exclude_patterns = ['_build']
 
+# Extensions
+extensions = [
+    'sphinx_copybutton',
+    'sphinx_rtd_theme',
+]
+
 # Theme
 import sphinx_rtd_theme
-extensions = ['sphinx_rtd_theme']
 html_theme = 'sphinx_rtd_theme'
 html_theme_options = {
-    'canonical_url': 'https://jmd-dk.github.io/concept/',
-    'logo_only': False,
-    'display_version': False,
+    # General
+    'canonical_url'             : 'https://jmd-dk.github.io/concept/',
+    'display_version'           : False,
+    'logo_only'                 : False,
     'prev_next_buttons_location': 'both',
-    'style_external_links': True,
+    'style_external_links'      : True,
     # Navigation
-    'collapse_navigation': False,
-    'sticky_navigation': True,
-    'navigation_depth': -1,
-    'titles_only': False,
+    'collapse_navigation'        : False,
+    'navigation_depth'           : -1,
+    'sticky_navigation'          : True,
     'style_nav_header_background': 'darkorange',
+    'titles_only'                : False,
 }
 
 # HTML
-html_logo = html_static_path[0] + '/logo.png'
-html_favicon = html_static_path[0] + '/favicon.ico'
-html_title = 'CONCEPT Documentation'
-html_context = {'display_github': True}
-html_show_sourcelink = False
-html_show_sphinx = False
-html_show_copyright = False
-html_last_updated_fmt = None
-html_use_smartypants = True
+html_context           = {'display_github': True}
+html_favicon           = html_static_path[0] + '/favicon.ico'
+html_last_updated_fmt  = None
+html_logo              = html_static_path[0] + '/logo.png'
 html_scaled_image_link = False
+html_show_copyright    = False
+html_show_sourcelink   = False
+html_show_sphinx       = False
+html_title             = 'CONCEPT Documentation'
+html_use_smartypants   = True
+
